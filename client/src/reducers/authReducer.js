27d1ace1e7ce8a7e37d55c339f6d96f257d95572
sajicode <sajioloye@gmail.com>
2019-01-04 20:46:45 +0100

    test react router with in-app component

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,10 +1,6 @@
-import { FETCH_USER } from '../actions/types';
-
-export default function(state = null, action) {
-  switch (action.type) {
-    case FETCH_USER:
-      return action.payload || false;
-    default:
-      return state;
-  }
+export default function(state = {}, action) {
+	switch (action.type) {
+		default:
+			return state;
+	}
 }
