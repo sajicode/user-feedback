3f7b58d7464a11bf733458e8aa49447ae2343b2f
sajicode <sajioloye@gmail.com>
2019-01-05 21:15:09 +0100

    display header content based on auth status

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export class Header extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return 'Still thinking';
+			case false:
+				return 'Get Out';
+
+			default:
+				return 'Welcome';
+		}
+	}
+
 	render() {
 		return (
 			<nav>
 				<div className="nav-wrapper">
 					<a className="left brand-logo">Emaily</a>
-					<ul className="right">
-						<li>
-							<a>Login With Google</a>
-						</li>
-					</ul>
+					<ul className="right">{this.renderContent()}</ul>
 				</div>
 			</nav>
 		);
 	}
 }
 
-export default Header;
+const mapStateToProps = ({ auth }) => {
+	return {
+		auth: auth
+	};
+};
+
+export default connect(mapStateToProps)(Header);
