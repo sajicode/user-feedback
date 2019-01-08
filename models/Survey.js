b45dded1c1e1a6e9cc22797aec92dcdf52c81476
sajicode <sajioloye@gmail.com>
2019-01-08 02:50:00 +0100

    set up recipient schema, update survey schema

--- models/Survey.js
+++ models/Survey.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
 	title: String,
 	body: String,
 	subject: String,
-	recipients: [ String ],
+	recipients: [ RecipientSchema ],
 	yes: {
 		type: Number,
 		default: 0
@@ -13,7 +14,13 @@ const surveySchema = new Schema({
 	no: {
 		type: Number,
 		default: 0
-	}
+	},
+	_user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	dateSent: Date,
+	lastResponded: Date
 });
 
 mongoose.model('surveys', surveySchema);
