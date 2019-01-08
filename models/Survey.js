ef726a0bafb10aab71be29529e6f792a88a0f4ab
sajicode <sajioloye@gmail.com>
2019-01-08 02:33:59 +0100

    create Survey model

--- models/Survey.js
+++ models/Survey.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const RecipientSchema = require('./Recipient');
 
 const surveySchema = new Schema({
-  title: String,
-  body: String,
-  subject: String,
-  recipients: [RecipientSchema],
-  yes: { type: Number, default: 0 },
-  no: { type: Number, default: 0 },
-  _user: { type: Schema.Types.ObjectId, ref: 'User' },
-  dateSent: Date,
-  lastResponded: Date
+	title: String,
+	body: String,
+	subject: String,
+	recipients: [ String ],
+	yes: {
+		type: Number,
+		default: 0
+	},
+	no: {
+		type: Number,
+		default: 0
+	}
 });
 
 mongoose.model('surveys', surveySchema);
