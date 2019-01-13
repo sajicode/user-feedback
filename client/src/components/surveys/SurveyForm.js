9a750fb3a4f5b86b1f96c36a34305955bb8f761d
sajicode <sajioloye@gmail.com>
2019-01-13 18:48:08 +0100

    setup generic form validation

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -40,9 +40,14 @@ class SurveyForm extends Component {
 function validate(values) {
 	const errors = {};
 
-	if (!values.title) {
-		errors.title = 'You must provide a title';
-	}
+	// if (!values.title) {
+	// 	errors.title = 'You must provide a title';
+	// }
+	_.each(FIELDS, ({ name }) => {
+		if (!values[name]) {
+			errors[name] = `You must provide a ${name}`;
+		}
+	});
 
 	return errors;
 }
