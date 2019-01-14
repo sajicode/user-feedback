b384a3bcf873c6ecaade07d7bd8541ce23618168
sajicode <sajioloye@gmail.com>
2019-01-14 04:05:19 +0100

    setup redirect after posting survey using withRouter

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import _ from 'lodash';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import formFields from './formFields';
 import * as actions from '../../actions';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history }) => {
 	const reviewFields = _.map(formFields, ({ name, label }) => {
 		return (
 			<div key={name}>
@@ -22,7 +23,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey }) => {
 			<button className="yellow darken-3 white-text btn-flat" onClick={onCancel}>
 				Back
 			</button>
-			<button className="green btn-flat right white-text" onClick={() => submitSurvey(formValues)}>
+			<button className="green btn-flat right white-text" onClick={() => submitSurvey(formValues, history)}>
 				Send Survey
 				<i className="material-icons right">email</i>
 			</button>
@@ -37,7 +38,9 @@ function mapStateToProps(state) {
 	};
 }
 
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
 
 // form values in mapstatetoprops are from the redux store form
 // redux actions are also mapped to props and thus accessible via props
+
+// withRouter aids in redirecting our page after posting, it provides the history api inside props which we send to the action creator for use
