520889e8c58d327a351bf562a017d34228b89159
sajicode <sajioloye@gmail.com>
2019-01-14 01:23:54 +0100

    refactor fields array and setup formReview fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -5,18 +5,12 @@ import { reduxForm, Field } from 'redux-form';
 import { Link } from 'react-router-dom';
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-	{ label: 'Survey Title', name: 'title' },
-	{ label: 'Subject Line', name: 'subject' },
-	{ label: 'Email Body', name: 'body' },
-	{ label: 'Recipient List', name: 'emails' }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
 	renderFields() {
 		// iterate over FIELDS, 2nd parameter is each field destructured
-		return _.map(FIELDS, ({ label, name }) => {
+		return _.map(formFields, ({ label, name }) => {
 			return <Field key={name} component={SurveyField} type="text" label={label} name={name} />;
 		});
 	}
@@ -46,7 +40,7 @@ function validate(values) {
 	// }
 	errors.emails = validateEmails(values.emails || '');
 
-	_.each(FIELDS, ({ name }) => {
+	_.each(formFields, ({ name }) => {
 		if (!values[name]) {
 			errors[name] = `You must provide a value`;
 		}
@@ -57,9 +51,12 @@ function validate(values) {
 
 export default reduxForm({
 	validate,
-	form: 'surveyForm'
+	form: 'surveyForm',
+	destroyOnUnmount: false
 })(SurveyForm);
 
 // the surveyfield component receives label as a prop alongside other redux-form props
 
 // validate is for validation from redux-form
+
+// destroyOnUmnount persists form input
