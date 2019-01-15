bb0352b20c40a101f1b1958fcdab1c6cc8dd2914
sajicode <sajioloye@gmail.com>
2019-01-16 00:33:35 +0100

    fetch list of surveys by current user

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,6 +10,13 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
+	app.get('/api/surveys', requireLogin, async (req, res) => {
+		// return suveys excluding recipients
+		const surveys = await Survey.find({ _user: req.user.id }).select({ recipients: false });
+
+		res.send(surveys);
+	});
+
 	app.get('/api/surveys/:surveyId/:choice', (req, res) => {
 		res.send('Thanks for your response');
 	});
