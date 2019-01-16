2ff60cc819fc289d23f59fa2844fd2b9c8292232
sajicode <sajioloye@gmail.com>
2019-01-16 01:10:16 +0100

    reverse surveys list to display latest first

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -8,7 +8,7 @@ class SurveyList extends Component {
 	}
 
 	renderSurveys() {
-		return this.props.surveys.map((survey) => {
+		return this.props.surveys.reverse().map((survey) => {
 			return (
 				<div className="card darken-1 blue-grey" key="survey._id">
 					<div className="card-content white-text">
